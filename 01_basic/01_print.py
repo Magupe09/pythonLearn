0d2c416c26e9dab3e12bc86246bfb56fca57267d
Mauricio <mgp.94@hotmail.com>
2025-01-21 18:15:34 -0500

    pirnt()

---
+++ 01_basic/01_print.py
@@ -0,0 +1,6 @@
+print("hola mundo") 
+print("python","es","brutal")
+print("python","es","brutal",sep="-")
+print("python","es","brutal",end=" ")
+
+print("python","es","brutal")
---
+++ 01_basic/README.md
@@ -0,0 +1,10 @@
+# curso de python desde cero @Midudev
+
+## Primera clase:
+-hola mundo
+-tipos de datos
+-conversion de tipos
+-variables
+-Numbers
+-Booleanos
+-strings
