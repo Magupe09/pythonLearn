ee1d68eb730d2568405eea0fe93bf1fb30aa36ec
Mauricio <mgp.94@hotmail.com>
2025-02-02 19:57:29 -0500

    "Resuelvo ejercicios de listas"

--- 02_flow_control/03_list.py
+++ 02_flow_control/03_list.py
@@ -3,7 +3,7 @@
 # Secuencias mutables de elementos.
 # Pueden contener elementos de diferentes tipos.
 ###
-
+"""
 import os
 os.system("clear")
 
@@ -28,7 +28,7 @@ print(matrix)
 print("\nAcceso a elementos por índice")
 print(lista2[0])  # manzanas
 print(lista2[1])  # peras
-print(lista2[-1]) # plátanos
+print(lista2[-1]) # plátanos
 print(lista2[-2]) # peras
 
 print(lista_de_listas[1][0])
@@ -62,7 +62,9 @@ print(lista1)
 
 # Recuperar longitud de una lista
 print("Longitud de la lista", len(lista1))
-
+"""
+import os
+os.system("clear") 
 ###
 # EJERCICOS
 ###
@@ -72,28 +74,50 @@ print("Longitud de la lista", len(lista1))
 # mensaje = ["C", "o", "d", "i", "g", "o", " ", "s", "e", "c", "r", "e", "t", "o"]
 # Utilizando slicing y concatenación, crea una nueva lista que contenga solo el mensaje "secreto".
 
+#Soluciones 
+
+# mensajeSecreto = mensaje[:6] + mensaje[7:14]
+# print(mensajeSecreto)
+
 # Ejercicio 2: Intercambio de posiciones
 # Dada la siguiente lista:
-# numeros = [10, 20, 30, 40, 50]
+#numeros = [10, 20, 30, 40, 50]
 # Intercambia la primera y la última posición utilizando solo asignación por índice.
+#copia_numeros = numeros[:]
+#numeros[4]=numeros[0]
+#numeros[0]=copia_numeros[4]
+#print(numeros)
 
 # Ejercicio 3: El sándwich de listas
 # Dadas las siguientes listas:
-# pan = ["pan arriba"]
-# ingredientes = ["jamón", "queso", "tomate"]
-# pan_abajo = ["pan abajo"]
+#pan = ["pan arriba"]
+#ingredientes = ["jamón", "queso", "tomate"]
+#pan_abajo = ["pan abajo"]
 # Crea una lista llamada sandwich que contenga el pan de arriba, los ingredientes y el pan de abajo, en ese orden.
+
+#sandwich = pan + ingredientes + pan_abajo
+#print(sandwich)
 
 # Ejercicio 4: Duplicando la lista
 # Dada una lista:
-# lista = [1, 2, 3]
+#lista = [1, 2, 3]
 # Crea una nueva lista que contenga los elementos de la lista original duplicados.
 # Ejemplo: [1, 2, 3] -> [1, 2, 3, 1, 2, 3]
+
+#copia_list= lista[:] + lista
+#print(copia_list)
 
 # Ejercicio 5: Extrayendo el centro
 # Dada una lista con un número impar de elementos, extrae el elemento que se encuentra en el centro de la lista utilizando slicing.
 # Ejemplo: lista = [10, 20, 30, 40, 50] -> El centro es 30
+#lista = [10, 20, 30, 40, 50]
+#elemento_central = lista[len(lista)//2 : len(lista)//2 + 1][0]
+#print(elemento_central)
+
 
 # Ejercicio 6: Reversa parcial
 # Dada una lista, invierte solo la primera mitad de la lista (utilizando slicing y concatenación).
 # Ejemplo: lista = [1, 2, 3, 4, 5, 6] -> Resultado: [3, 2, 1, 4, 5, 6]
+lista = [1, 2, 3, 4, 5, 6]
+nueva_lista = lista[0:3]
+print(nueva_lista[::-1]+lista[3:]) 
