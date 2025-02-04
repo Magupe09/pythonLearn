c6934a7d346e29d9d6a9234636d511cfea8201ae
Mauricio <mgp.94@hotmail.com>
2025-02-04 17:48:03 -0500

    "solucion ejercicio 1 y 2"

--- 03_loops/01_loop_while.py
+++ 03_loops/01_loop_while.py
@@ -2,7 +2,7 @@
 # 01 - Bucles (while)
 # Permiten ejecutar un bloque de código repetidamente mientras se cumpla una condición
 ###
-
+"""
 print("\n Bucle while:")
 
 # Bucle con una simple condición
@@ -73,6 +73,10 @@ while numero < 0:
 
 print(f"El número que has introducido es {numero}")
 
+
+
+import os
+os.system("clear")
 ###
 # EJERCICIOS (while)
 ###
@@ -80,31 +84,64 @@ print(f"El número que has introducido es {numero}")
 # Ejercicio 1: Cuenta atrás
 # Imprime los números del 10 al 1 usando un bucle while.
 print("\nEjercicio 1:")
+contador = 10
+
+while contador <= 10:
+    print(contador)
+    contador -=1
+    if contador == 0:
+     break
 
 # Ejercicio 2: Suma de números pares (while)
 # Calcula la suma de los números pares entre 1 y 20 (inclusive) usando un bucle while.
-print("\nEjercicio 2:")
+#print("\nEjercicio 2:")
+
+contador = 1
+pares=0
+while contador <= 20:
+    
+    contador +=1
+
+    if contador % 2 == 0:
+        pares += contador
+print(pares)
+
+"""
 
 # Ejercicio 3: Factorial de un número
 # Pide al usuario que introduzca un número entero positivo.
 # Calcula su factorial usando un bucle while.
 # El factorial de un número entero positivo es el producto de todos los números del 1 al ese número. Por ejemplo, el factorial de 5
 # 5! = 5 x 4 x 3 x 2 x 1 = 120.
-print("\nEjercicio 3:")
+#print("\nEjercicio 3:")
+import os
+os.system("clear")
+factor = int (input('Ingrese un numero entero positivo'))
+contador = 1
+
+while factor >= 1:
+    contador = factor * factor-1
+    print(contador)
+    if factor ==1:
+        break
+    print(factor)  
+    factor-=1
+  
+print(factor,contador)
 
 # Ejercicio 4: Validación de contraseña
 # Pide al usuario que introduzca una contraseña.
 # La contraseña debe tener al menos 8 caracteres.
 # Usa un bucle while para seguir pidiendo la contraseña hasta que cumpla con los requisitos.
 # Si la contraseña es válida, imprime "Contraseña válida".
-print("\nEjercicio 4:")
+#print("\nEjercicio 4:")
 
 # Ejercicio 5: Tabla de multiplicar
 # Pide al usuario que introduzca un número.
 # Imprime la tabla de multiplicar de ese número (del 1 al 10) usando un bucle while.
-print("\nEjercicio 5:")
+#print("\nEjercicio 5:")
 
 # Ejercicio 6: Números primos hasta N
 # Pide al usuario que introduzca un número entero positivo N.
 # Imprime todos los números primos menores o iguales que N usando un bucle while.
-print("\nEjercicio 6:")
+#print("\nEjercicio 6:")
